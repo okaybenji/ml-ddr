cce57f8def15461199fc780a7fc3e8ef5c949fdb
Benji Kay <okaybenji@gmail.com>
2017-07-09 15:00:25 -0500

    create/use saveSim module

---
+++ id3.js
@@ -0,0 +1,60 @@
+const fs = require('fs');
+const id3 = require('jsmediatags');
+const mp3Duration = require('mp3-duration');
+const saveSim = require('./saveSim');
+
+const mp3 = 'test.mp3';
+const img = 'test.png';
+const sim = 'test.sm';
+
+const getMetadata = () => new Promise((onSuccess, onError) => {
+  id3.read(mp3, {onSuccess, onError});
+});
+
+const getSize = (metadata) => new Promise(resolve => {
+  metadata.bytes = fs.statSync(mp3).size;
+  resolve(metadata);
+});
+
+const getLength = (metadata) => new Promise((resolve, reject) => {
+  mp3Duration(mp3, (err, duration) => {
+    if (err) {
+      return reject(err);
+    }
+    metadata.duration = duration;
+    resolve(metadata);
+  });
+});
+
+getMetadata()
+  .then(getSize)
+  .then(getLength)
+  .then(metadata => {
+    const songInfo = {
+      title: metadata.tags.title,
+      artist: metadata.tags.artist,
+      banner: img,
+      background: img,
+      album: metadata.tags.album,
+      music: mp3,
+      bytes: metadata.bytes,
+      duration: metadata.duration
+    };
+
+    saveSim(songInfo, sim);
+
+    // This doesn't quite work. Not sure why yet.
+//    const image = tags.picture.data.reduce((acc, cur) => {
+//      acc += String.fromCharCode(cur);
+//
+//      return acc;
+//    }, '');
+//
+//    fs.writeFile(img, image, 'base64', (err) => {
+//      if (err) {
+//        return console.log(err);
+//      }
+//      console.log('image saved');
+//    });
+  });
+
--- index.js
+++ index.js
@@ -1,89 +1,19 @@
-const brain = require('brain.js');
-const net = new brain.NeuralNetwork();
+const spotifier = require('./spotify');
+const saveImage = require('./saveImage');
+const saveSim = require('./saveSim');
 
-const perf = net.train([{input: { r: 0.03, g: 0.7, b: 0.5 }, output: { black: 1 }},
-           {input: { r: 0.16, g: 0.09, b: 0.2 }, output: { white: 1 }},
-           {input: { r: 0.5, g: 0.5, b: 1.0 }, output: { white: 1 }}]);
+const songPath = 'test.mp3';
+const artPath = 'test.jpg';
 
-const output = net.run({ r: 1, g: 0.4, b: 0 })
-
-// console.log(perf);
-// console.log(output.white > output.black ? 'white' : 'black');
-
-/* - */
-
-const mp3 = 'test.mp3';
-const img = 'test.png';
-const fs = require('fs');
-const id3 = require('jsmediatags');
-const mp3Duration = require('mp3-duration');
-
-const getMetadata = () => new Promise((onSuccess, onError) => {
-  id3.read(mp3, {onSuccess, onError});
-});
-
-const getSize = (metadata) => new Promise(resolve => {
-  metadata.byteSize = fs.statSync(mp3).size;
-  resolve(metadata);
-});
-
-const getLength = (metadata) => new Promise((resolve, reject) => {
-  mp3Duration(mp3, (err, duration) => {
-    if (err) {
-      return reject(err);
-    }
-    metadata.duration = duration;
-    resolve(metadata);
-  });
-});
-
-getMetadata()
-  .then(getSize)
-  .then(getLength)
-  .then(metadata => {
-    const tags = metadata.tags;
-    const output = `
-      #TITLE:${tags.title};
-      #SUBTITLE:;
-      #ARTIST:${tags.artist};
-      #TITLETRANSLIT:;
-      #SUBTITLETRANSLIT:;
-      #ARTISTTRANSLIT:;
-      #CREDIT: Made with ml-ddr;
-      #BANNER:${img};
-      #BACKGROUND:${img};
-      #CDTITLE:${tags.album};
-      #MUSIC:${mp3};
-      #MUSICBYTES:${metadata.byteSize};
-      #MUSICLENGTH:${metadata.duration};
-      #OFFSET:;
-      #SAMPLESTART:;
-      #SAMPLELENGTH:;
-      #SELECTABLE:YES;
-      #BPMS:0.000=;
-      #STOPS:;
-      #BGCHANGES:;
-    `;
-
-    fs.writeFile('test.sm', output, (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log('simfile saved');
-    });
-
-    // This doesn't quite work. Not sure why yet.
-    const image = tags.picture.data.reduce((acc, cur) => {
-      acc += String.fromCharCode(cur);
-
-      return acc;
-    }, '');
-
-    fs.writeFile(img, image, 'base64', (err) => {
-      if (err) {
-        return console.log(err);
-      }
-      console.log('image saved');
-    });
-  });
-
+spotifier
+  .getSongInfo('linda ronstadt don\'t know much cry like a rainstorm')
+  .then((songInfo) => {
+    const simData = Object.assign({
+      filename: songPath,
+      banner: artPath,
+      background: artPath
+    }, songInfo);
+    saveSim(simData, 'test.sm');
+    saveImage(songInfo.art, artPath);
+  })
+  .catch(err => console.log(err));
