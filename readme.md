6862e5df30a5d78fa5315b634d54062fb7ee8fcc
Benji Kay <okaybenji@gmail.com>
2017-07-09 15:29:17 -0500

    add some documentation

---
+++ readme.md
@@ -0,0 +1,6 @@
+The only thing this really does right now is retrieve song info (including tempo and album art) from Spotify and save it to a simfile which you can then add steps to. That's kinda handy, though!
+
+Edit the index.js file, then run:
+```
+node index
+```
--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ const spotifier = require('./spotify');
 const saveImage = require('./saveImage');
 const saveSim = require('./saveSim');
 
-const song = 'test.mp3';
-const art = 'test.jpg';
-const sim = 'test.sm';
+// Name these however you like.
+const song = 'test.mp3'; // You'll provide this mp3.
+const art = 'test.jpg'; // I'll retrieve the art and save to this filename.
+const sim = 'test.sm'; // I'll generate this file as a template for you.
+const query = 'linda ronstadt don\'t know much cry like a rainstorm'; // Try to be specific.
 
 spotifier
-  .getSongInfo('linda ronstadt don\'t know much cry like a rainstorm')
+  .getSongInfo(query)
   .then((songInfo) => {
     const simData = Object.assign({
       filename: song,
       banner: art,
       background: art
     }, songInfo);
+    // Save to the 'output' folder.
     saveSim(simData, `../output/${sim}`);
     saveImage(songInfo.art, `../output/${art}`);
   })
